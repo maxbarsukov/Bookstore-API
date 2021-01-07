eedfef6a9cc623d96ec2908d9025fbd40e48dc84
maxbarsukov <maximbarsukov@bk.ru>
2021-01-07 21:50:18 +0300

    Tests added

---
+++ spec/requests/books_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Books API' do
+  it 'returns all books' do
+    FactoryBot.create(:book, title: '1984', author: 'George Orwell')
+    FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G Wells')
+
+    get '/api/v1/books'
+
+    expect(response).to have_http_status(:success)
+    expect(JSON.parse(response.body).size).to eq(2)
+  end
+end
