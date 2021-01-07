8ea7c7d619c1ab6509006d02b22a44e00011b146
maxbarsukov <maximbarsukov@bk.ru>
2021-01-07 20:09:34 +0300

    CRUD for books

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -7,6 +7,47 @@ module Api
         books = Book.order('created_at DESC')
         render json: { status: 'SUCCESS', message: 'Loaded books', data: books }, status: :ok
       end
+
+      def show
+        book = Book.find(params[:id])
+        render json: { status: 'SUCCESS', message: 'Loaded book', data: book }, status: :ok
+      end
+
+      def create
+        book = Book.new(book_params)
+        if book.save
+          render json: { status: 'SUCCESS', message: 'Saved book', data: book }, status: :ok
+        else
+          render json: { status: 'ERROR',
+                         message: 'Book not saved',
+                         data: book.errors },
+                 status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        render json: { status: 'SUCCESS', message: 'Deleted book', data: book }, status: :ok
+      end
+
+      def update
+        book = Book.update(params[:id], book_params)
+        if book.save
+          render json: { status: 'SUCCESS', message: 'Updated book', data: book }, status: :ok
+        else
+          render json: { status: 'ERROR',
+                         message: 'Book not updated',
+                         data: book.errors },
+                 status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def book_params
+        params.permit(:title, :author)
+      end
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2021_01_07_164938) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
