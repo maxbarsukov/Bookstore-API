12c7d8f9c236bdf4845522a39f915c518ece91c3
maxbarsukov <maximbarsukov@bk.ru>
2021-01-09 11:10:01 +0300

    DB and seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,9 @@
 5.times do
   Book.create({
                 title: Faker::Book.title,
-                author: Faker::Book.title
+                author: Author.create({
+                                        first_name: Faker::Name.first_name,
+                                        last_name: Faker::Name.first_name
+                                      })
               })
 end
